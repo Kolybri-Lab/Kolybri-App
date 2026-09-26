405b0c88c90f3b46578d0a4ddfa830d5f768409e
As de Pique <as2pick.card@outlook.fr>
2026-09-26 17:57:53 +0200

    goto 1.1.0

--- app.config.js
+++ app.config.js
@@ -72,7 +72,7 @@ export default {
         policy: "appVersion",
     },
 
-    version: "1.0.0",
+    version: "1.1.0",
     orientation: "portrait",
     icon: "./assets/icons/icon.png",
     userInterfaceStyle: "automatic",
@@ -103,5 +103,5 @@ export default {
         },
     },
     owner: "kolybrilab",
-    githubUrl: "https://github.com/Kolybri-Lab/EcoleDirectePlus-Mobile",
+    githubUrl: "https://github.com/Kolybri-Lab/Kolybri-App",
 };
